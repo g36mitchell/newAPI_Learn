e233b7f6c80ed2d3ba6664cc9f38d2de2d44155e
Gail Mitchell <g36mitchell@gamil.com>
2019-10-16 20:49:24 -0500

    final version tested

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const apiKey = "34d1a42bdf8e475f87431b19afe09609";
+const appKey = "g36mitchell";
 
-const searchURL = 'https://newsapi.org/v2/everything';
-
+const searchURL = 'https://api.github.com/users';
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -11,42 +10,55 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-function displayResults(responseJson, maxResults) {
+function displayResults(responseJson, maxResults, username) {
   // if there are previous results, remove them
   console.log(responseJson);
+  $('#results-avatar').empty();
   $('#results-list').empty();
   // iterate through the articles array, stopping at the max number of results
-  for (let i = 0; i < responseJson.articles.length & i<maxResults ; i++){
-    // for each video object in the articles
-    //array, add a list item to the results 
-    //list with the article title, source, author,
-    //description, and image
-    $('#results-list').append(
-      `<li><h3><a href="${responseJson.articles[i].url}">${responseJson.articles[i].title}</a></h3>
-      <p>${responseJson.articles[i].source.name}</p>
-      <p>By ${responseJson.articles[i].author}</p>
-      <p>${responseJson.articles[i].description}</p>
-      <img src='${responseJson.articles[i].urlToImage}'>
-      </li>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
+  
+  if (responseJson.length != 0) {
+         $('#results-avatar').append(`<img src="${responseJson[0].owner.avatar_url}" alt="avatar"></img>`);
+          for (let i = 0; i < responseJson.length & i< maxResults ; i++){
+               $('#results-list').append(
+                 `<li><h3><a href="${responseJson[i].html_url}" target="_blank" title="${username}">${responseJson[i].full_name}</a></h3>
+                 <p>Description: ${responseJson[i].description}</p>
+                 <p>Last update: ${responseJson[i].updated_at}</p>
+                 <p>Watchers: ${responseJson[i].watchers}</p>
+                 <p>Language: ${responseJson[i].language}</p>
+                 </li>`
+                   )};
+             //display the results section  
+    }
+    else {
+        $('#results-list').append(`<p>No repositories found for this handle.</p>`);
+    }
+
+    $('#results').removeClass('hidden');
+
 };
 
-function getNews(query, maxResults=10) {
+function getRepo(query) {
   const params = {
-    q: query,
-    language: "en",
+    "sort": "updated"
   };
   const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
+  const maxResults = 10;
+  const url = `${searchURL}/${query}/repos?${queryString}`;
 
   console.log(url);
 
+ 
   const options = {
     headers: new Headers({
-      "X-Api-Key": apiKey})
+      "User-Agent": appKey,
+      "Accept": "application/vnd.github.v3+json"})
   };
+
+  $('#results').addClass('hidden');
+  $('#results-avatar').empty();
+  $('#results-list').empty(); 
+  $('#js-error-message').empty();
 
   fetch(url, options)
     .then(response => {
@@ -55,9 +67,11 @@ function getNews(query, maxResults=10) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson, maxResults))
+    .then(responseJson => displayResults(responseJson, maxResults, query)
+ /*         console.log(responseJson)  */
+    )
     .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      $('#js-error-message').text(`Oops! GitHub says: ${err.message}`);
     });
 }
 
@@ -65,8 +79,7 @@ function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
-    getNews(searchTerm, maxResults);
+    getRepo(searchTerm);
   });
 }
 
